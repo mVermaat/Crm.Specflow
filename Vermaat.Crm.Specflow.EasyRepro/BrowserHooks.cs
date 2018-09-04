df6a3cd28a0942dbcfcf5e6489e720fe956f95b7
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2018-09-04 16:44:35 +0200

    EasyRepro initial

---
+++ Vermaat.Crm.Specflow.EasyRepro/BrowserHooks.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Vermaat.Crm.Specflow.EasyRepro
+{
+    [Binding]
+    public class BrowserHooks
+    {
+        private readonly SeleniumTestingContext _seleniumTestingContext;
+
+        public BrowserHooks(SeleniumTestingContext seleniumTestingContext)
+        {
+            _seleniumTestingContext = seleniumTestingContext;
+        }
+
+        [BeforeScenario("Chrome")]
+        public void ChromeSetup()
+        {
+            _seleniumTestingContext.BrowserOptions.BrowserType = BrowserType.Chrome;
+        }
+
+        [BeforeScenario("Edge")]
+        public void EdgeSetup()
+        {
+            _seleniumTestingContext.BrowserOptions.BrowserType = BrowserType.Edge;
+        }
+
+        [BeforeScenario("Firefox")]
+        public void FirefoxSetup()
+        {
+            _seleniumTestingContext.BrowserOptions.BrowserType = BrowserType.Firefox;
+        }
+
+        [BeforeScenario("IE")]
+        public void IESetup()
+        {
+            _seleniumTestingContext.BrowserOptions.BrowserType = BrowserType.IE;
+        }
+    }
+}
