03ecbeecf8eb341e4e5b3e1bd228e7a8f32db832
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2019-04-16 14:39:01 +0200

    Added legacy to assembly name

--- Vermaat.Crm.Specflow/Properties/AssemblyInfo.cs
+++ Vermaat.Crm.Specflow/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vermaat.Crm.Specflow")]
+[assembly: AssemblyTitle("Vermaat.Crm.Specflow.Legacy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vermaat.Crm.Specflow")]
+[assembly: AssemblyProduct("Vermaat.Crm.Specflow.Legacy")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
