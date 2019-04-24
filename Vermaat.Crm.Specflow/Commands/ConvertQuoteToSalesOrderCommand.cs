26316ba3a6051aaf033ba378208414861de9caef
Higo Cidario <higocidario@outlook.com>
2019-04-24 20:40:28 +0200

    Added activate,win and convert quote to sales order steps

---
+++ Vermaat.Crm.Specflow/Commands/ConvertQuoteToSalesOrderCommand.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Vermaat.Crm.Specflow.Commands
+{
+    public class ConverQuoteToSalesOrderCommand : ApiOnlyCommandFunc<Entity>
+    {
+        private readonly string _alias;
+
+        public ConverQuoteToSalesOrderCommand(CrmTestingContext crmContext, string alias) : base(crmContext)
+        {
+            _alias = alias;
+        }
+
+        public override Entity Execute()
+        {
+            var aliasRef = _crmContext.RecordCache[_alias];
+
+            var activateQuote = new ConvertQuoteToSalesOrderRequest()
+            {
+                QuoteId = aliasRef.Id,
+                ColumnSet = new ColumnSet(true),
+            };
+
+            return _crmContext.Service.Execute<ConvertQuoteToSalesOrderResponse>(activateQuote).Entity;
+        }
+    }
+}
---
+++ Vermaat.Crm.Specflow/Steps/QuoteSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Vermaat.Crm.Specflow.Commands;
+
+namespace Vermaat.Crm.Specflow.Steps
+{
+    [Binding]
+    public class QuoteSteps
+    {
+        private readonly CrmTestingContext _crmContext;
+
+        public QuoteSteps(CrmTestingContext crmContext)
+        {
+            _crmContext = crmContext;
+        }
+
+        [When(@"a quote named (.*) is activated")]
+        public void ActivateQuote(string alias)
+        {
+            _crmContext.CommandProcessor.Execute(new ActivateQuoteCommand(_crmContext, alias));
+        }
+
+        [When(@"a quote named (.*) is converted to a sales order")]
+        public void ConverQuoteToSalesOrder(string alias)
+        {
+            _crmContext.CommandProcessor.Execute(new ConverQuoteToSalesOrderCommand(_crmContext, alias));
+        }
+
+        [When(@"a quote named (.*) is won")]
+        public void WinQuote(string alias)
+        {
+            _crmContext.CommandProcessor.Execute(new WinQuoteCommand(_crmContext, alias));
+        }
+    }
+}
