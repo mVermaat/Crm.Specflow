dc8243f165e7ef3aaa7923eb7b4b6f2ea6436147
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2019-04-25 09:29:50 +0200

    Added quote steps

---
+++ Vermaat.Crm.Specflow/Commands/WinQuoteCommand.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermaat.Crm.Specflow.Commands
+{
+    public class WinQuoteCommand : ApiOnlyCommandFunc<WinQuoteResponse>
+    {
+        private readonly string _alias;
+
+        public WinQuoteCommand(CrmTestingContext crmContext, string alias) : base(crmContext)
+        {
+            _alias = alias;
+        }
+
+        public override WinQuoteResponse Execute()
+        {
+            var aliasRef = _crmContext.RecordCache[_alias];
+
+            var quoteClose = new Entity("quoteclose");
+            quoteClose.Attributes.Add("subject", "Quote Close " + DateTime.Now.ToString());
+            quoteClose.Attributes.Add("quoteid", aliasRef);
+
+            var winQuoteRequest = new WinQuoteRequest()
+            {
+                QuoteClose = quoteClose,
+                Status = new OptionSetValue(4)
+            }; 
+
+            return _crmContext.Service.Execute<WinQuoteResponse>(winQuoteRequest);
+        }
+    }
+}
