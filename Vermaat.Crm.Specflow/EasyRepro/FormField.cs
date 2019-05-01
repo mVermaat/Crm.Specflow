9f2b19b4b39b1cca3b6723b26ffbcda7e1c07b09
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2019-05-01 10:14:55 +0200

    Changed setting value to properly clear fields

--- Vermaat.Crm.Specflow/EasyRepro/FormField.cs
+++ Vermaat.Crm.Specflow/EasyRepro/FormField.cs
@@ -200,23 +200,20 @@ namespace Vermaat.Crm.Specflow.EasyRepro
                     if (input != null)
                     {
                         var currentValue = input.GetAttribute("value");
-                        var stringBuilder = new StringBuilder((value?.Length).GetValueOrDefault() + currentValue.Length + 2);
-                        if (!string.IsNullOrEmpty(currentValue))
-                        {
-                            for (int i = 0; i < currentValue.Length; i++)
-                            {
-                                stringBuilder.Append(Keys.Backspace);
-                            }
-                        }
-
-                        if(!string.IsNullOrWhiteSpace(value)) 
-                            stringBuilder.Append(value);
-
-                        stringBuilder.Append(Keys.Tab);
-                        stringBuilder.Append(Keys.Tab);
 
                         input.Click();
-                        input.SendKeys(stringBuilder.ToString());
+                        if (!string.IsNullOrWhiteSpace(currentValue))
+                        {
+                            input.SendKeys(Keys.Control + "a");
+                            input.SendKeys(Keys.Backspace);
+                        }
+
+                        if(!string.IsNullOrWhiteSpace(value))
+                        {
+                            input.SendKeys(value);
+                        }
+
+                        input.SendKeys(Keys.Tab + Keys.Tab);
                     }
                 }
                 else if (fieldContainer.FindElements(By.TagName("textarea")).Count > 0)
