48568f0a0fe8542359dd812ae159b9757bbb75a3
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2019-06-14 10:50:55 +0200

    Set logger to public

--- Vermaat.Crm.Specflow/Logger.cs
+++ Vermaat.Crm.Specflow/Logger.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vermaat.Crm.Specflow
 {
-    static class Logger
+    public static class Logger
     {
         public static void WriteLine(string text)
         {
