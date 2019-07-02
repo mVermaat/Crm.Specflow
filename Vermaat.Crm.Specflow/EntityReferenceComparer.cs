44bea0cbaebdb193a24cbb9f6e3a8737405b9472
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2019-07-02 12:23:16 +0200

    Added helper for entityreference comparison

---
+++ Vermaat.Crm.Specflow/EntityReferenceComparer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermaat.Crm.Specflow
+{
+    public class EntityReferenceComparer : IEqualityComparer<EntityReference>
+    {
+        public bool Equals(EntityReference first, EntityReference second)
+        {
+            if (ReferenceEquals(first, second))
+            {
+                return true;
+            }
+            if (ReferenceEquals(first, null) ||
+                ReferenceEquals(second, null))
+            {
+                return false;
+            }
+            return first.Id == second.Id;
+        }
+
+        public int GetHashCode(EntityReference obj)
+        {
+            if (obj == null)
+                return 0;
+            return obj.GetHashCode();
+        }
+    }
+}
