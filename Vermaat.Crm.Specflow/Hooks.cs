e89d9b3b7876f4e29ef96fe7eefffac607a1a9c1
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2019-08-20 08:53:44 +0200

    Added hook to take a sceenshot when a test fails

--- Vermaat.Crm.Specflow/Hooks.cs
+++ Vermaat.Crm.Specflow/Hooks.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Dynamics365.UIAutomation.Browser;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +15,16 @@ namespace Vermaat.Crm.Specflow
     {
         private readonly SeleniumTestingContext _seleniumContext;
         private readonly CrmTestingContext _crmContext;
+        private readonly FeatureContext _featureContext;
+        private readonly ScenarioContext _scenarioContext;
 
-        public Hooks(SeleniumTestingContext seleniumTestingContext, CrmTestingContext crmContext)
+        public Hooks(SeleniumTestingContext seleniumTestingContext, CrmTestingContext crmContext,
+                     FeatureContext featureContext,  ScenarioContext scenarioContext)
         {
             _seleniumContext = seleniumTestingContext;
             _crmContext = crmContext;
+            _featureContext = featureContext;
+            _scenarioContext = scenarioContext;
         }
 
         [AfterScenario("Cleanup")]
@@ -88,6 +95,55 @@ namespace Vermaat.Crm.Specflow
         {
             GlobalTestingContext.BrowserManager.Dispose();
             GlobalTestingContext.ConnectionManager.Dispose();
+        }
+
+        [AfterScenario]
+        public void AfterWebTest()
+        {
+            if (_scenarioContext.TestError != null && !_crmContext.IsTarget("API"))
+            {
+                TakeScreenshot(_seleniumContext.GetBrowser().App.WebDriver);
+            }
+        }
+
+        private void TakeScreenshot(IWebDriver driver)
+        {
+            try
+            {
+                string fileNameBase = string.Format("error_{0}_{1}_{2}",
+                                                    _featureContext.FeatureInfo.Title,
+                                                    _scenarioContext.ScenarioInfo.Title,
+                                                    DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+
+                var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+                if(!directory.Name.Equals("TestResults", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    directory = directory.CreateSubdirectory("TestResults");
+                }
+                Logger.WriteLine($"Screenshot path: {directory.FullName}");
+
+                string pageSource = driver.PageSource;
+                string sourceFilePath = Path.Combine(directory.FullName, fileNameBase + "_source.html");
+                File.WriteAllText(sourceFilePath, pageSource, Encoding.UTF8);
+                Console.WriteLine("Page source: {0}", new Uri(sourceFilePath));
+
+                ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+
+                if (takesScreenshot != null)
+                {
+                    var screenshot = takesScreenshot.GetScreenshot();
+
+                    string screenshotFilePath = Path.Combine(directory.FullName, fileNameBase + "_screenshot.png");
+
+                    screenshot.SaveAsFile(screenshotFilePath, ScreenshotImageFormat.Png);
+
+                    Logger.WriteLine($"Screenshot: {new Uri(screenshotFilePath)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLine($"Error while taking screenshot: {ex}");
+            }
         }
     }
 }
