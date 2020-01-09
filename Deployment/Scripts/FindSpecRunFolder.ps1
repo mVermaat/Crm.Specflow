4e1822bf7a1d65f32f0cbb7bbae97155cf78d2b1
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2020-01-09 09:17:29 +0100

    Added scripts to build pipeline

---
+++ Deployment/Scripts/FindSpecRunFolder.ps1
@@ -0,0 +1,10 @@
+$result = Get-ChildItem -Path "$(system.defaultworkingdirectory)\packages" -Directory -Filter "SpecRun.Runner.*"
+
+if($result.Length -gt 0) {
+    Write-Host "Setting variable to $($result[0].FullName)"
+    Write-Host "##vso[task.setvariable variable=SpecRunFolder;]$($result[0].FullName)"  
+}
+else {
+    Write-Host "SpecRun foulder not found"
+    exit 1;
+}
