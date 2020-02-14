c91d2315a88052d772586e9b83fa7ab050f50d22
Martijn Vermaat <martijn.vermaat@dynamichands.nl>
2020-02-14 12:24:43 +0100

    Fixes

--- Vermaat.Crm.Specflow/Connectivity/ConnectionManager.cs
+++ Vermaat.Crm.Specflow/Connectivity/ConnectionManager.cs
@@ -17,7 +17,10 @@ namespace Vermaat.Crm.Specflow.Connectivity
         public BrowserLoginDetails CurrentBrowserLoginDetails => _currentConnection.GetBrowserLoginInformation();
         public BrowserLoginDetails CurrentLoginDetails { get; private set; }
 
-
+        public ConnectionManager()
+        {
+            _connectionCache = new Dictionary<string, CrmService>();
+        }
 
         public void SetAdminConnection(CrmConnection connection)
         {
@@ -42,51 +45,6 @@ namespace Vermaat.Crm.Specflow.Connectivity
                 connection.Service = service;
             }
         }
-
-
-        //public CrmService CurrentConnection { get; private set; }
-        //public CrmService AdminConnection { get; private set; }
-        //public UserDetails CurrentUserDetails { get; private set; }
-        //public Uri Url { get; }
-
-        //public void SetAdminConnection(string identifier)
-        //{
-        //    ConnectionCache connectionCache = GetConnectionCache(connection, "admin");
-        //    AdminConnection = connectionCache.Service;
-        //}
-
-        //public void SetCurrentConnection(string identifier)
-        //{
-        //    ConnectionCache connectionCache = GetConnectionCache(connection, "current");
-        //    CurrentConnection = connectionCache.Service;
-        //    CurrentUserDetails = connectionCache.UserDetails;
-        //}
-
-        //public ConnectionManager()
-        //{
-        //    Url = new Uri(HelperMethods.GetAppSettingsValue("Url"));
-        //    _connectionCache = new Dictionary<string, ConnectionCache>();
-        //}
-
-
-        //private ConnectionCache GetConnectionCache(CrmConnection connection, string connectionType)
-        //{
-        //    Logger.WriteLine($"Changing {connectionType} connection to {connection.UserDetails.Username}");
-        //    if (!_connectionCache.TryGetValue(connection.UserDetails.Username, out ConnectionCache connectionCache))
-        //    {
-        //        Logger.WriteLine("Connection doesn't exist. Creating new API connection");
-        //        connectionCache = new ConnectionCache
-        //        {
-        //            Service = new CrmService(connection.GetConnectionString()),
-        //            UserDetails = new UserDetails { Username = connection.UserDetails.Username, 
-        //                Password = connection.UserDetails.Password }
-        //        };
-        //        connectionCache.UserDetails.UserSettings = UserSettings.GetUserSettings(connectionCache.Service, connection.UserDetails.Username);
-        //        _connectionCache.Add(connection.UserDetails.Username, connectionCache);
-        //    }
-
-        //    return connectionCache;
-        //}
 
         #region IDisposable Support
         private bool _disposedValue = false;
