4b80935768426d2a441f1a4bbe4019b6f976c380
Martijn Vermaat <13309338+mVermaat@users.noreply.github.com>
2020-02-26 13:40:09 +0100

    Features/driverspath (#52)
    * Driver path will now be retrieved from environment variable if possible
    * Driver path default to null so a custom path will be selected if the implementor doesn't set it

--- Vermaat.Crm.Specflow/SeleniumTestingContext.cs
+++ Vermaat.Crm.Specflow/SeleniumTestingContext.cs
@@ -1,13 +1,4 @@
-﻿using Microsoft.Dynamics365.UIAutomation.Api;
-using Microsoft.Dynamics365.UIAutomation.Browser;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Dynamics365.UIAutomation.Browser;
 using Vermaat.Crm.Specflow.EasyRepro;
 
 namespace Vermaat.Crm.Specflow
@@ -29,7 +20,7 @@ namespace Vermaat.Crm.Specflow
                 CleanSession = true,
                 StartMaximized = true,
                 UCITestMode = true,
-                DriversPath = GetDriverPath()
+                DriversPath = null,
 
             };
             CurrentApp = HelperMethods.GetAppSettingsValue("AppName", true);
@@ -45,13 +36,6 @@ namespace Vermaat.Crm.Specflow
             browser.ChangeApp(CurrentApp);
             IsLoggedIn = true;
             return browser;
-        }
-
-        private string GetDriverPath()
-        {
-            var assemblyPath = new FileInfo(Assembly.GetExecutingAssembly().Location);
-            Logger.WriteLine($"Using chrome driver path: {assemblyPath.Directory}");
-            return assemblyPath.DirectoryName;
         }
 
     }
