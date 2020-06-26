e4869e8885f919e43af0dc57a94ef2c49421e6b5
Martijn Vermaat <13309338+mVermaat@users.noreply.github.com>
2020-06-26 09:58:05 +0200

    Deletes records from new to old (#99)
    * Deletes records from new to old
    
    * fixed cache
    
    Co-authored-by: Martijn Vermaat <martijn.vermaat@dynamichands.nl>

---
+++ Vermaat.Crm.Specflow/AliasedRecord.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermaat.Crm.Specflow
+{
+    class AliasedRecord
+    {
+        public DateTime CreatedOn { get; }
+        public string Alias { get; }
+        public EntityReference Record { get; set; }
+
+        public AliasedRecord(string alias, EntityReference record)
+        {
+            Record = record;
+            CreatedOn = DateTime.Now;
+        }
+    }
+}
