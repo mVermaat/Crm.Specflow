98ba963b01d22cfcfd7945f77256681d72739ebe
Martijn Vermaat <mvermaat@hso.com>
2022-02-08 09:07:56 +0100

    updated for oauth

--- Deployment/Scripts/CreateAppSettings.ps1
+++ Deployment/Scripts/CreateAppSettings.ps1
@@ -5,12 +5,9 @@ param(
 
 ######################### Script #########################
 $output = "<appSettings>" +
-"<add key=""AuthType"" value="""" />" +
 "<add key=""Url"" value="""" />" +
 "<add key=""Username"" value="""" />" +
 "<add key=""Password"" value="""" />" +
-"<add key=""ClientId"" value="""" />" +
-"<add key=""ClientSecret"" value="""" />" +
 "</appSettings>"
 
 Out-File -FilePath $outputpath -InputObject $output
