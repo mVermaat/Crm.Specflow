4aaa91d10cca462c2e5955af46c0712a62fd5644
Martijn Vermaat <mvermaat@hso.com>
2022-02-14 16:31:23 +0100

    keyvault WIP

---
+++ Vermaat.Crm.Specflow/Entities/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermaat.Crm.Specflow.Entities
+{
+    public class UserProfile
+    {
+        public string Username { get; set; }
+        public string Profile { get; set; }
+    }
+}
---
+++ Vermaat.Crm.Specflow/Steps/AuthenticationSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Vermaat.Crm.Specflow.Steps
+{
+    [Binding]
+    public class AuthenticationSteps
+    {
+        private readonly CrmTestingContext _crmContext;
+        private readonly SeleniumTestingContext _seleniumContext;
+
+        public AuthenticationSteps(CrmTestingContext crmContext, SeleniumTestingContext seleniumContext)
+        {
+            _crmContext = crmContext;
+            _seleniumContext = seleniumContext;
+        }
+
+        [Given(@"A logged in '(.*)' named ([^\s]+)")]
+        public void LoginWithUser()
+        {
+
+        }
+    }
+}
