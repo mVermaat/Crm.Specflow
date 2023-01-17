067aadffe684443b1b0cdd84c631b701dbe19a18
Martijn Vermaat <mvermaat@hso.com>
2023-01-17 09:35:49 +0100

    temp selecttab

--- Vermaat.Crm.Specflow/EasyRepro/FormState.cs
+++ Vermaat.Crm.Specflow/EasyRepro/FormState.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Dynamics365.UIAutomation.Browser;
+﻿using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Vermaat.Crm.Specflow.EasyRepro
@@ -41,10 +44,69 @@ namespace Vermaat.Crm.Specflow.EasyRepro
             if (string.IsNullOrEmpty(CurrentTab) || !CurrentTab.Equals(tabLabel, StringComparison.OrdinalIgnoreCase))
             {
                 Logger.WriteLine($"Expanding tab {tabLabel}");
-                _app.App.Entity.SelectTab(tabLabel);
+                SelectTab(tabLabel);
                 CurrentTab = tabLabel;
             }
             
+        }
+
+        private void SelectTab(string tabName)
+        {
+            Thread.Sleep(2000);
+
+            _app.Client.Execute($"Select Tab", driver =>
+            {
+                IWebElement tabList;
+                if (driver.HasElement(By.XPath(AppElements.Xpath[AppReference.Dialogs.DialogContext])))
+                {
+                    var dialogContainer = driver.FindElement(By.XPath(AppElements.Xpath[AppReference.Dialogs.DialogContext]));
+                    tabList = dialogContainer.WaitUntilAvailable(By.XPath(AppElements.Xpath[AppReference.Entity.TabList]));
+                }
+                else
+                {
+                    tabList = driver.WaitUntilAvailable(By.XPath(AppElements.Xpath[AppReference.Entity.TabList]));
+                }
+
+                ClickTab(driver, tabList, AppElements.Xpath[AppReference.Entity.Tab], tabName);
+
+                driver.WaitForTransaction();
+                return true;
+            });
+        }
+
+        private void ClickTab(IWebDriver driver, IWebElement tabList, string xpath, string name)
+        {
+            IWebElement moreTabsButton;
+            IWebElement listItem;
+            // Look for the tab in the tab list, else in the more tabs menu
+            IWebElement searchScope = null;
+            if (tabList.HasElement(By.XPath(string.Format(xpath, name))))
+            {
+                searchScope = tabList;
+            }
+            else if (tabList.TryFindElement(By.XPath(AppElements.Xpath[AppReference.Entity.MoreTabs]), out moreTabsButton))
+            {
+                moreTabsButton.Click();
+
+                // No tab to click - subtabs under 'Related' are automatically expanded in overflow menu
+                if (name == "Related")
+                {
+                    return;
+                }
+                else
+                {
+                    searchScope = driver.FindElement(By.XPath(AppElements.Xpath[AppReference.Entity.MoreTabsMenu]));
+                }
+            }
+
+            if (searchScope.TryFindElement(By.XPath(string.Format(xpath, name)), out listItem))
+            {
+                listItem.Click(true);
+            }
+            else
+            {
+                throw new Exception($"The tab with name: {name} does not exist");
+            }
         }
     }
 }
