0888dc5a4254a42b9c82653c02603e516fb33c49
Martijn Vermaat <mvermaat@hso.com>
2023-05-11 16:01:49 +0200

    ribbon for flyout

--- Vermaat.Crm.Specflow/EasyRepro/Commands/GetRibbonItemCommand.cs
+++ Vermaat.Crm.Specflow/EasyRepro/Commands/GetRibbonItemCommand.cs
@@ -7,16 +7,29 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow.CommonModels;
 
 namespace Vermaat.Crm.Specflow.EasyRepro.Commands
 {
     public class GetRibbonItemCommand : ISeleniumCommandFunc<IWebElement>
     {
-        private readonly string _buttonName;
+        private readonly string _subButtonName;
+        private readonly string _flyoutParentButtonName;
 
         public GetRibbonItemCommand(string buttonName)
         {
-            _buttonName = buttonName;
+            if(string.IsNullOrEmpty(buttonName))
+                throw new ArgumentNullException(nameof(buttonName));
+
+            if (buttonName.Contains('.'))
+            {
+                _flyoutParentButtonName = buttonName.Substring(0, buttonName.IndexOf('.'));
+                _subButtonName = buttonName.Substring(buttonName.IndexOf('.') + 1);
+            }
+            else
+            {
+                _subButtonName = buttonName;
+            }
         }
 
         public virtual CommandResult<IWebElement> Execute(BrowserInteraction browserInteraction)
@@ -33,8 +46,28 @@ namespace Vermaat.Crm.Specflow.EasyRepro.Commands
                     return CommandResult<IWebElement>.Fail(true, Constants.ErrorCodes.RIBBON_NOT_FOUND, "main");
             }
 
+            if(string.IsNullOrEmpty(_flyoutParentButtonName))
+            {
+                return GetRibbonButton(browserInteraction, _subButtonName, ribbon);
+            }
+            {
+                var result = GetRibbonButton(browserInteraction, _flyoutParentButtonName, ribbon);
+                if (!result.IsSuccessfull)
+                    return result;
+
+                if (result.Result.TryFindElement(browserInteraction.Selectors.GetXPathSeleniumSelector(SeleniumSelectorItems.Entity_Ribbon_Button, _subButtonName), out var subItem))
+                    return CommandResult<IWebElement>.Success(subItem);
+                else
+                    return CommandResult<IWebElement>.Success(null);
+            }
+
+
+        }
+
+        private CommandResult<IWebElement> GetRibbonButton(BrowserInteraction browserInteraction, string buttonName, IWebElement ribbon)
+        {
             // Find in regular buttons, return if found
-            if (ribbon.TryFindElement(browserInteraction.Selectors.GetXPathSeleniumSelector(SeleniumSelectorItems.Entity_Ribbon_Button, _buttonName), out var item))
+            if (ribbon.TryFindElement(browserInteraction.Selectors.GetXPathSeleniumSelector(SeleniumSelectorItems.Entity_Ribbon_Button, buttonName), out var item))
                 return CommandResult<IWebElement>.Success(item);
 
             // Not found, look for more commands button
@@ -49,13 +82,12 @@ namespace Vermaat.Crm.Specflow.EasyRepro.Commands
 
 
                 // Find in more commands list
-                if (ribbon.TryFindElement(browserInteraction.Selectors.GetXPathSeleniumSelector(SeleniumSelectorItems.Entity_Ribbon_Button, _buttonName), out item))
+                if (ribbon.TryFindElement(browserInteraction.Selectors.GetXPathSeleniumSelector(SeleniumSelectorItems.Entity_Ribbon_Button, buttonName), out item))
                     return CommandResult<IWebElement>.Success(item);
             }
 
             // Item isn't in the ribbon
             return CommandResult<IWebElement>.Success(null);
-
-        }        
+        }
     }
 }
