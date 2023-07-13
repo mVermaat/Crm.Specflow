3ce0711cc4f812b0ea70297380dcdb4f919beb3d
Martijn Vermaat <mvermaat@hso.com>
2023-07-13 14:05:22 +0200

    fixed error

--- Vermaat.Crm.Specflow/ErrorCodes.cs
+++ Vermaat.Crm.Specflow/ErrorCodes.cs
@@ -84,7 +84,7 @@ namespace Vermaat.Crm.Specflow
             AddError(Constants.ErrorCodes.CHECK_ACCESS_BUTTON_NOT_VISIBLE, "Check Access button isn't available");
             AddError(Constants.ErrorCodes.CHECK_ACCESS_DIALOG_NOT_FOUND, "Check Access dialog not found");
             AddError(Constants.ErrorCodes.CHECK_ACCESS_DIALOG_UNKNOWN_ICON, "Unknown Check Access permission icon: '{0}'");
-            AddError(Constants.ErrorCodes.CHECK_ACCESS_DIALOG_MISSING_PERMISSIONS, "Check Access permission {0} missing. Available permissions: {1}");
+            AddError(Constants.ErrorCodes.CHECK_ACCESS_DIALOG_MISSING_PERMISSIONS, "Check Access permission missing. Available permissions: {1}");
             AddError(Constants.ErrorCodes.CHECK_ACCESS_WRONG_ACCESS_EXPECTATION_TEXT, "Error parsing expected access data. Input was {0}. Allowed possibilities: {1}");
             AddError(Constants.ErrorCodes.CHECK_ACCESS_ERRORS_FOUND, "{0} Permission errors found: {1}");
         }
