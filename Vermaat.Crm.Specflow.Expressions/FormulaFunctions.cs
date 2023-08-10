65800339b4d09961c45277d210e22558a4270625
Martijn Vermaat <mvermaat@hso.com>
2023-08-10 13:01:22 +0200

    added formulas

--- Vermaat.Crm.Specflow.Expressions/FormulaFunctions.cs
+++ Vermaat.Crm.Specflow.Expressions/FormulaFunctions.cs
@@ -13,6 +13,21 @@ namespace Vermaat.Crm.Specflow.Expressions
             return DateTime.Today;
         }
 
+        public static int CurrentDay()
+        {
+            return DateTime.Today.Day;
+        }
+
+        public static int CurrentMonth()
+        {
+            return DateTime.Today.Month;
+        }
+
+        public static int CurrentYear()
+        {
+            return DateTime.Today.Year;
+        }
+
         public static TimeSpan Days(double amount)
         {
             return TimeSpan.FromDays(amount);
